e76c313a2b166086c0b350ff368beea12009d969
yuvraj-05828 <yuvraj05828@gmail.com>
2024-02-10 14:58:12 +0530

    updated

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,18 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    console.log(images)
+    const [i,setI]=useState(0)
+    return (
+        <div className="carouselcontainer" style={{backgroundImage:`url(${images[i].img})`}}>
+            <div id="leftarrow" onClick={()=>{setI(i=>(i+2)%3)}}><ArrowBackIosIcon/></div>
+                <div className="container">
+                    <h1>{images[i].title}</h1>
+                    <h3>{images[i].subtitle}</h3>
+                </div>
+            <div id="rightarrow" onClick={()=>{setI(i=>(i+1)%3)}}><ArrowForwardIosIcon/></div>
+        </div>
+      )
 }
 
 export default Carousel;
